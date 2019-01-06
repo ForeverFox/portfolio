13742cfdec11499ffe95ffd2dbf9c553412e9b89
Forever Fox <cogden@mcad.edu>
2019-01-07 00:03:29 +0200

    Updates

--- js/projects.js
+++ js/projects.js
@@ -1,27 +1,30 @@
 $("document").ready(function() {    
   $("#web").click(function(){
-    $(".print, .illustration, .photography, .video, .audio, .ux").hide(); $(".web").show();
+    $(".print, .illustration, .photography, .type,  .video, .audio, .ux").hide(); $(".web").show();
   });
   $("#ux").click(function(){
-    $(".web, .print, .illustration, .photography, .audio, .video").hide(); $(".ux").show();
+    $(".web, .print, .illustration, .photography, .type,  .audio, .video").hide(); $(".ux").show();
   });   
   $("#print").click(function(){
-    $(".web, .illustration, .photography, .video, .audio, .ux").hide(); $(".print").show();
-  });  
+    $(".web, .illustration, .photography, .type, .video, .audio, .ux").hide(); $(".print").show();
+  }); 
+  $("#type").click(function(){
+    $(".web, .illustration, .photography, .print, .video, .audio, .ux").hide(); $(".type").show();
+  }); 
   $("#illustration").click(function(){
-   $(".web, .print, .photography, .video, .audio, .ux").hide(); $(".illustration").show();
+   $(".web, .print, .photography, .type, .video, .audio, .ux").hide(); $(".illustration").show();
   });
   $("#photography").click(function(){
-   $(".web, .print, .illustration, .video, .audio, .ux").hide(); $(".photography").show();
+   $(".web, .print, .illustration, .type, .video, .audio, .ux").hide(); $(".photography").show();
   });
   $("#video").click(function(){
-    $(".web, .print, .illustration, .photography, .audio, .ux").hide(); $(".video").show();
+    $(".web, .print, .illustration, .photography, .type,  .audio, .ux").hide(); $(".video").show();
   }); 
   $("#audio").click(function(){
-    $(".web, .print, .illustration, .photography, .video, .ux").hide(); $(".audio").show();
+    $(".web, .print, .illustration, .photography, .type,  .video, .ux").hide(); $(".audio").show();
   });  
 
   $("#showAll").click(function(){
-   $(".web, .print, .illustration, .photography, .video, .audio, .ux").show();
+   $(".web, .print, .illustration, .photography, .type,  .video, .audio, .ux").show();
   }); 
 });
