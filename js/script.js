23ecef3aa86f5492da677f652d584ff6b7917306
Forever Fox <cogden@mcad.edu>
2019-01-16 21:06:34 +0200

    Preloading

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+
+	$(function() {	
+
+		$("#preload-images").waitForImages({
+		    finished: function() {
+		        console.log("All images loaded.");
+				$("#preloader").delay(250).fadeOut(500);
+				$("#home").addClass("loaded");			
+			},
+		    each: function() {
+	       		console.log("Image loaded.")
+		    },
+		    waitForAll: true
+		});	
+	});
+
 	$('.popup-gallery').magnificPopup({
 		delegate: 'a',
 		type: 'image',
